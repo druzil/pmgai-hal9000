41ccf6f126cd4e1beb6bfc6817f5c7d050ca2c33
Darryl Stein <darrylstein@hotmail.com>
2015-11-14 14:40:46 +1100

    added text to speech

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
-#
+﻿#
 # This file is part of The Principles of Modern Game AI.
 # Copyright (c) 2015, AiGameDev.com KG.
 #
 
 import vispy                    # Main application support.
-
+import nltk.chat
 import window                   # Terminal input and display.
-
+import win32com.client
+import speech_recognition as sr
 
 class HAL9000(object):
     
+    AGENT_RESPONSES = [
+      (r'You are (worrying|scary|disturbing)',    # Pattern 1.
+        ['Yes, I am %1.',                         # Response 1a.
+         'Oh, sooo %1.']),
+
+      (r'Are you ([\w\s]+)\?',                    # Pattern 2.
+        ["Why would you think I am %1?",          # Response 2a.
+         "Would you like me to be %1?"]),
+
+      (r'',                                       # Pattern 3. (default)
+        ["Is everything OK?",                     # Response 3a.
+         "Can you still communicate?"])
+    ]
+
+    first = True
+
     def __init__(self, terminal):
         """Constructor for the agent, stores references to systems and initializes internal memory.
         """
         self.terminal = terminal
         self.location = 'unknown'
+        self.chatbot = nltk.chat.Chat(self.AGENT_RESPONSES, nltk.chat.util.reflections)
+        self.voice = win32com.client.Dispatch("SAPI.SpVoice")
+        self.recog = sr.Recognizer()
+        self.recog.energy_threshold = 1000           # Tune based on ambient noise levels [1000,4000].
 
     def on_input(self, evt):
         """Called when user types anything in the terminal, connected via event.
         """
-        self.terminal.log("Good morning! This is HAL.", align='right', color='#00805A')
+        if evt.text == "Where am I?":
+            self.terminal.log("On the Starship Entreprise.", align='right', color='#00805A')
+        else:
+            if self.first:
+                self.terminal.log("Howdy! I'm HAL.", align='right', color='#00805A')
+                self.voice.Speak('Good afternoon! My name is Hal.')
+                self.first = False
+            else:
+                self.voice.Speak(evt.text)
+                resp1 = self.chatbot.respond(evt.text)
+                self.terminal.log(resp1, align='right', color='#00805A')
 
     def on_command(self, evt):
         """Called when user types a command starting with `/` also done via events.
@@ -29,7 +60,9 @@ class HAL9000(object):
 
         elif evt.text.startswith('relocate'):
             self.terminal.log('', align='center', color='#404040')
-            self.terminal.log('\u2014 Now in the {}. \u2014'.format(evt.text[9:]), align='center', color='#404040')
+            self.location = evt.text[9:]
+            self.terminal.log('\u2014 Now in the {}. \u2014'.format(self.location), align='center', color='#404040')
+             
 
         else:
             self.terminal.log('Command `{}` unknown.'.format(evt.text), align='left', color='#ff3000')    
@@ -38,6 +71,15 @@ class HAL9000(object):
     def update(self, _):
         """Main update called once per second via the timer.
         """
+        #with sr.Microphone() as source:     # Use the default microphone as the audio source.
+        #    audio = self.recog.listen(source)        # Listen for single phrase and extract as audio.
+
+        #try:
+        #    text = recog.recognize_google(audio)       # Extract text using Google Speech Recognition.
+        #    #print(text)
+        #    self.terminal.log(text, align='right', color='#00805A')
+        #except LookupError:
+        #    text = None                     # Could not recognize anything from audio...
         pass
 
 
